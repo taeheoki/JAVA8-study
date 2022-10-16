be5a5505b74a7ae35d779ec5723f3857eeb7006f
taeheoki <dkqjwl2365@naver.com>
2022-10-16 22:20:40 +0900

    [Lamda]feat: Variable Capture

---
+++ src/main/java/test/VariableCapture.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+
+public class VariableCapture {
+
+    private void run() {
+        int baseNumber = 10; //effective final variable
+        //final int baseNumber = 10; //Java 8 부터 final 생략 가능
+
+        //로컬 클래스
+        class LocalClass {
+            void printBaseNumber(){
+//                baseNumber = 12;
+                System.out.println(baseNumber);
+            }
+        }
+
+        //익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(baseNumber);
+            }
+        };
+
+        //람다
+        IntConsumer printInt = (i) -> System.out.println(i + baseNumber);
+        printInt.accept(10);
+
+    }
+
+}
