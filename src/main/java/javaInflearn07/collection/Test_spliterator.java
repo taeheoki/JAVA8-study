801c55655ff1fa6c643a1b1b6857f7e4a9e862df
taeheoki <dkqjwl2365@naver.com>
2022-10-17 15:56:17 +0900

    [Java8 API]feat: spliterator in Collection

---
+++ src/main/java/javaInflearn07/collection/Test_spliterator.java
@@ -0,0 +1,18 @@
+package javaInflearn07.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
+public class Test_spliterator {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("taeheoki");
+        names.add("catsbi");
+        names.add("toby");
+        names.add("mijeong");
+
+        Spliterator<String> spliterator = names.spliterator();
+        while(spliterator.tryAdvance(System.out::println));
+    }
+}
