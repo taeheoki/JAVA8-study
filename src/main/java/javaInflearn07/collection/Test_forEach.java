8c8c7ec9332b0a1b00c56b43775c6cf310fffc5e
taeheoki <dkqjwl2365@naver.com>
2022-10-17 15:53:34 +0900

    [Java8 API]feat: forEach in Collection

---
+++ src/main/java/javaInflearn07/collection/Test_forEach.java
@@ -0,0 +1,21 @@
+package javaInflearn07.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test_forEach {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("taeheoki");
+        names.add("catsbi");
+        names.add("toby");
+        names.add("mijeong");
+
+        names.forEach(System.out::println);
+
+        /* for(String name : names) {
+             System.out.println(name);
+           }
+         */
+    }
+}
