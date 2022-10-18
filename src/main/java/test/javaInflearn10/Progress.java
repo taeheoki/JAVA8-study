e848e15ee7e1f8ebb075cb56a878bfa499b9070f
taeheoki <dkqjwl2365@naver.com>
2022-10-18 21:28:24 +0900

    [JAVA8 Optional]feat: Optional 등장 배경

---
+++ src/main/java/test/javaInflearn10/Progress.java
@@ -0,0 +1,17 @@
+package test.javaInflearn10;
+
+import java.time.Duration;
+
+public class Progress {
+    public Duration studyDuration;
+
+    private boolean finished;
+
+    public Duration getStudyDuration() {
+        return studyDuration;
+    }
+
+    public void setStudyDuration(Duration studyDuration) {
+        this.studyDuration = studyDuration;
+    }
+}
